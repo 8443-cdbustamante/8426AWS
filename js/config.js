8901e3d25001cb836f59b6960c903a2b8f9db755
CristianBst <cdbustamante@espe.edu.ec>
2022-11-28 14:24:02 -0500

    1.1
    creacion de cliente cognito

---
+++ js/config.js
@@ -0,0 +1,15 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_KFXpiHubO',
+        userPoolClientId: '40mmdg9iij62l2g1kv0lt6bn36',
+        region: 'us-east-1'
+    },
+    cognito: {
+        userPoolId: 'us-east-1_KFXpiHubO',
+        userPoolClientId: '39d9mt6kd7v3gkgl4nfc0ffkoh',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
